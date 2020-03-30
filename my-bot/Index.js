584204b246e55ab2607a2d40ea916ab1ccebd7a3
fire1706 <46523392+fire1706@users.noreply.github.com>
2020-03-30 23:46:59 +0200

    commit

--- my-bot/Index.js
+++ my-bot/Index.js
@@ -1,4 +1,3 @@
-require("dotenv").config()
 const Discord = require("discord.js")
 const client = new Discord.Client()
 client.on("ready", () => {
@@ -8,5 +7,13 @@ client.on("message", msg => {
   if (msg.content === "ping") {
     msg.reply("Pong!")
   }
+client.on("message", msg => {
+  if (msg.content === "Hey") {
+    msg.reply("Slt slt mon petit")
+  }
+client.on("message", msg => {
+  if (msg.content === "Thomas est le meilleur !") {
+    msg.reply("C'est tout à fait vrai.")
+  }
 })
-client.login(process.env.BOT_TOKEN)
+client.login("NjkyMzkzMzc5NzQwMjU0MjIw.XoJjBw.v-mZ3ifd5l9HPSTWqLrG2gI7Shc")
