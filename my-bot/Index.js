b97e56cb9472da7a0b3c81e6f0e1dcb46938dfb7
fire1706 <46523392+fire1706@users.noreply.github.com>
2020-03-31 13:06:56 +0200

    Update Index.js

--- my-bot/Index.js
+++ my-bot/Index.js
@@ -7,8 +7,11 @@ bot.on("message", msg => {
   if (msg.content === "ping") {
     msg.reply("Pong!")
   }
-  if (msg.content === "slt") {
+  if (msg.content.includes("slt")) {
     msg.reply("Hey Bonjour!")
+  }
+  if (msg.content.includes("cool")) {
+    msg.reply("Tu l'as dis mon bonhomme ! ;)")
   }
   if (msg.content === "tic") {
     msg.reply("Tac")
@@ -61,8 +64,8 @@ bot.on("message", msg => {
   if (msg.content ==="Polo's Band"){msg.reply("Your mission,\nshould you choose to accept it,\n is to find Lindsey and bring her home.\nI''ve already assembled a team.\n I have them standing by,\n awaiting your word.\n This message, let's call it \n my excellent engagement gift to you, \n will self-destruct in five seconds. \n Good luck, Ethan, and thanks again.")}
   if (msg.content ==="Pauline"){msg.reply("Cul de poule")}
   if (msg.content === "Les Basse") {     msg.reply("Sous Coté!!!")   }
-if (msg.content === "Qu'est ce qu'on veut?"){msg.reply("plus de solos!!!") }
-if (msg.content ==="Hey le bot ça va?"){msg.reply("Bien et toi ? mon petit!")}
+  if (msg.content === "Qu'est ce qu'on veut?"){msg.reply("plus de solos!!!") }
+  if (msg.content ==="Hey le bot ça va?"){msg.reply("Bien et toi ? mon petit!")}
 
 
 
