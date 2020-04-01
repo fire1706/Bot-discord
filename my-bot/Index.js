60847d3dba91d98cebb7d07368057ff9d704d03a
fire1706 <46523392+fire1706@users.noreply.github.com>
2020-04-01 21:20:09 +0200

    d

--- my-bot/Index.js
+++ my-bot/Index.js
@@ -90,7 +90,6 @@ bot.on("message", msg => {
   if (msg.content === "!GuessNumber"){
     msg.reply("Ok, alors jouons!\n Voici les règles : j'ai choisi un chiffres entre 1 et 100 et tu vas devoir le deviner , à chaque fois je te dirai si tu est trop haut ou trop bas ;) \n Pour plus de facilité , tu devras formuler ton message comme ceci : xx \n Bon jeu ");
     var jean = guessnumber(bot);
-
   }
 
 })
@@ -103,8 +102,8 @@ function guessnumber(bot){
     var i = 0;
     let randomNumber = Math.floor(Math.random() * 100) + 1;
     bot.on("message", msg => {
-      if(parseInt(msg.content)==randomNumber){msg.reply("Bravo c'est gagné !")}
-      else if(msg.content === "!n_xx"){msg.reply("Tu te crois marrant , p'tit con va !");resetBot(msg.channel); return 1;}
+      if(parseInt(msg.content)==randomNumber){msg.reply("Bravo c'est gagné !");resetBot(msg.channel); return 1;}
+      else if(msg.content === "!n_xx"){msg.reply("Tu te crois marrant , p'tit con va !")}
       else if(parseInt(msg.content) < randomNumber){msg.reply("T'es un peu trop faible mon gas !")}
       else if(parseInt(msg.content) > randomNumber){msg.reply("T'es un peu trop haut mon gas !")}
   })
